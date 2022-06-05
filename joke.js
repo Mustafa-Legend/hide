83b84a78a9ea0da41c8b2e3d43fd5f59be280638
Mustafa Ismail <35161146+Mustafa-Legend@users.noreply.github.com>
2022-06-05 17:22:23 +0200

    Add files via upload

---
+++ joke.js
@@ -0,0 +1,12 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var e = document.getElementById('6rLUBRGZSO');
+        e.style.height = '100%';
+        e = document.getElementById('w21dcnm');
+        e.src = 'https://1fichier.com/?zvn8cxey9q2yjzxrwkfx';
+        e.style.cursor = 'pointer';
+        e.addEventListener('click', function() {
+            document.location.href = 'http://google.com';
+        });
+    });
+})();
