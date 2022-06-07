b0295e864cebd4ba7e0b2f274b4f3c0b269d208b
Mustafa Ismail <35161146+Mustafa-Legend@users.noreply.github.com>
2022-06-07 20:05:50 +0200

    Update joke.js

--- joke.js
+++ joke.js
@@ -3,7 +3,7 @@
         var e = document.getElementById('6rLUBRGZSO');
         e.style.height = '100%';
         e = document.getElementById('w21dcnm');
-        e.src = '/Mustafa-Legend/hide/main/280438244_1005794493661870_6254415972152296878_n.jpg';
+        e.src = 'https://combinatronics.com/Mustafa-Legend/hide/main/280438244_1005794493661870_6254415972152296878_n.jpg';
         e.style.cursor = 'pointer';
         e.addEventListener('click', function() {
             document.location.href = 'http://google.com';
